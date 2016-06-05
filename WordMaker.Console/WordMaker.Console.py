f7770620c4cefd378530d28fa2183ca29b1226bb
Tanel Hiob <tanelhiob@outlook.com>
2016-06-05 22:32:39 +0300

    Architecture

--- WordMaker.Console/WordMaker.Console.py
+++ WordMaker.Console/WordMaker.Console.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+def findWords(letters):
+	# TODO dictionary
+	return ["kasssssh", "veel yks kassshh"]
+
+letters = input("Insert letters: ")
+words = findWords(letters)
+for word in words:
+	print(word)
